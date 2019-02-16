6c548c852660cc7a5e4a30a576f297f1c24db7f5
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-16 13:16:54 +0000

    another API call but using async await instead of promises, much cleaner

--- api-calls.js
+++ api-calls.js
@@ -1,4 +1,8 @@
+// Fetch example
+
 const apiUrl = '';
+// pretending results will be parsed into an array of elements
+
 function getResults() {
     fetch(apiUrl)
     .then((results) => results.json())
@@ -9,3 +13,14 @@ function getResults() {
     })
 }
 getResults();
+
+// Api call example with async await instead of promises (still uses fetch)
+
+async function getResultsAsync() {
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    console.log(json[0]);
+}
+
+// clearly above example shows the power of async await & less lines of code
+
